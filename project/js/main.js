d89ba1fee73293c0522afe7b60127dbccac71e0e
Muzamil Khan <muzamilkhan5154@gmail.com>
2020-05-31 13:50:49 +0500

    weather api and navigation bar

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,36 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+// function showPicture(){
+//   // use jQuery ($ is shorthand) to find the div on the page and then change the html
+//   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+//   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
+//   $("p").html("High five! You're building your first web app!");
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+//   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
+
+// }
+$(document).ready(function() {
+    getWeather();
+})
+
+function getWeather(searchQuery) {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=metric&appid=" + apiKey;
+
+    $(".city").text("");
+    $(".temp").text("");
+
+    $.ajax(url, {
+        success: function(data) {
+            $(".city").text(data.name);
+            $(".temp").text(data.main.temp);
+
+        },
+        error: function() {
+            $(".error-message").text("An error occured");
+        }
+
+    })
+}
+
+function searchWeather() {
+    var searchQuery = $(".search").val();
+    getWeather(searchQuery);
 }
--- project/start.js
+++ project/start.js
@@ -10,11 +10,18 @@ app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
+});
+
+app.get('/about', function(request, response) {
+    response.render('pages/about');
+});
+app.get('/projects', function(request, response) {
+    response.render('pages/projects');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
