59ac37162c9b9024063b27f2f41caabc133b304e
Muzamil Khan <muzamilkhan5154@gmail.com>
2020-05-31 23:06:32 +0500

    Almost completed database app

--- project/js/main.js
+++ project/js/main.js
@@ -6,31 +6,53 @@
 
 //   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
 
+// // }
+// $(document).ready(function() {
+//     getWeather();
+// })
+
+// function getWeather(searchQuery) {
+//     var url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=metric&appid=" + apiKey;
+
+//     $(".city").text("");
+//     $(".temp").text("");
+
+//     $.ajax(url, {
+//         success: function(data) {
+//             $(".city").text(data.name);
+//             $(".temp").text(data.main.temp);
+
+//         },
+//         error: function() {
+//             $(".error-message").text("An error occured");
+//         }
+
+//     })
 // }
-$(document).ready(function() {
-    getWeather();
-})
 
-function getWeather(searchQuery) {
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=metric&appid=" + apiKey;
+// function searchWeather() {
+//     var searchQuery = $(".search").val();
+//     getWeather(searchQuery);
+// }
 
-    $(".city").text("");
-    $(".temp").text("");
 
-    $.ajax(url, {
-        success: function(data) {
-            $(".city").text(data.name);
-            $(".temp").text(data.main.temp);
-
-        },
-        error: function() {
-            $(".error-message").text("An error occured");
-        }
-
-    })
-}
-
-function searchWeather() {
-    var searchQuery = $(".search").val();
-    getWeather(searchQuery);
+function handleSignIn() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user.email);
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
 }
